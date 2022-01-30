9f61b7b5128162210cf33edefe6013b0ec1757b4
johndao1005 <john.d1729@gmail.com>
2022-01-30 14:35:23 +1300

    clean up and ready for project 2

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,27 +1,16 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Home from './pages/home/Home';
-import Search from './pages/search/Search';
-import Recipe from './pages/recipe/Recipe';
-import Create from './pages/create/Create';
-import Navbar from './components/Navbar';
-import ThemeSelector from './components/ThemeSelector';
-import { useTheme } from './hooks/useTheme';
+
 
 function App() {
-  const {mode} =useTheme()
-
+ 
   return (
-    <div className={`App ${mode}`} >
+    <div  >
       <BrowserRouter>
-      <Navbar/>
-      <ThemeSelector/>
+     
       <main>
         <Routes>
-          <Route path="/" element={<Home />} exact />
-          <Route path="/search" element={<Search /> } exact/>
-          <Route path="/recipes/:id" element={<Recipe exact/>} />
-          <Route path="/create" element={<Create />} exact/>
+       
         </Routes>
       </main>
       </BrowserRouter>
