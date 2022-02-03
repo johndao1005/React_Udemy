9bb1e7b1a11bd501e4c793ed3d61a00cc2824315
johndao1005 <john.d1729@gmail.com>
2022-02-04 09:46:18 +1300

    setting up firebase

--- front-end/src/pages/Home/Home.js
+++ front-end/src/pages/Home/Home.js
@@ -1,10 +1,23 @@
-import './Home.module.css'
-
+import styles from './Home.module.css'
+import {useAuthContext} from '../../hooks/useAuthContext'
+import {useCollections} from '../../hooks/useCollection'
 import React from 'react';
+import TransactionForm from './TransactionForm';
+import TransactionList from './TransactionList';
 
 
 export default function Home() {
-  return (<div>
-      Home
-  </div>);
+  const {user} = useAuthContext(); 
+  const {documents,error} = useCollections('transactions',["uid","==",user.uid],["createAt","desc"]);
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>
+      {error && <p>{error}</p>}
+      {documents && <TransactionList transactions={documents} />}
+      </div>
+      <div className={styles.sidebar}>
+        <TransactionForm uid={user.uid}/>
+      </div>
+      
+    </div>);
 }
