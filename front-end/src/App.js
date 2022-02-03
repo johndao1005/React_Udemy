eb437eb2a3f82f31f85b172a7cb93a32cd70c0a9
johndao1005 <john.d1729@gmail.com>
2022-02-04 11:00:38 +1300

    added project 1

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,34 +1,87 @@
-import './App.css';
-import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
-import Home from './pages/Home/Home';
-import Signup from './pages/Signup/Signup';
-import Login from './pages/Login/Login';
-import { useAuthContext } from './hooks/useAuthContext';
-import Navbar from './components/Navbar';
+import { useState, useEffect } from 'react'
+import './App.css'
+import SingleCard from './components/SingleCard'
 
+const cardImages = [
+  { "src": "/img/helmet-1.png", matched: false },
+  { "src": "/img/potion-1.png", matched: false },
+  { "src": "/img/ring-1.png", matched: false },
+  { "src": "/img/scroll-1.png", matched: false },
+  { "src": "/img/shield-1.png", matched: false },
+  { "src": "/img/sword-1.png", matched: false },
+]
 
 function App() {
-  const { authIsReady, user } = useAuthContext();
+  const [cards, setCards] = useState([])
+  const [turns, setTurns] = useState(0)
+  const [choiceOne, setChoiceOne] = useState(null)
+  const [choiceTwo, setChoiceTwo] = useState(null)
+
+  // shuffle cards for new game
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map(card => ({ ...card, id: Math.random() }))
+      
+    setCards(shuffledCards)
+    setTurns(0)
+  }
+
+  // handle a choice
+  const handleChoice = (card) => {
+    console.log(card)
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
+  }
+
+  // compare 2 selected cards
+  useEffect(() => {
+    if (choiceOne && choiceTwo) {
+
+      if (choiceOne.src === choiceTwo.src) {
+        setCards(prevCards => {
+          return prevCards.map(card => {
+            if (card.src === choiceOne.src) {
+              return { ...card, matched: true }
+            } else {
+              return card
+            }
+          })
+        })
+        resetTurn()
+      } else {
+        setTimeout(() => resetTurn(), 1000)
+      }
+
+    }
+  }, [choiceOne, choiceTwo])
+
+  console.log(cards)
+
+  // reset choices & increase turn
+  const resetTurn = () => {
+    setChoiceOne(null)
+    setChoiceTwo(null)
+    setTurns(prevTurns => prevTurns + 1)
+  }
+
   return (
-    <div  >
-      {authIsReady && ( // set this to enable the app to render when auth receive value of user or user is null
-        <BrowserRouter>
-          <Navbar />
-          <main>
-            <Routes>
-              <Route path="/" element={user ? <Home /> : <Navigate to="/login" />} />
+    <div className="App">
+      <h1>Magic Match</h1>
+      <button onClick={shuffleCards}>New Game</button>
 
-              <Route path="/signup"  element={!user ? <Signup /> : <Navigate to="/" />}/>
+      <div className="card-grid">
+        {cards.map(card => (
+          <SingleCard 
+            key={card.id}
+            card={card}
+            handleChoice={handleChoice}
+            flipped={card === choiceOne || card === choiceTwo || card.matched}
+          />
+        ))}
+      </div>
 
-              <Route path="/login"  element={!user ? <Login /> : <Navigate to="/" />}/>
-
-
-            </Routes>
-          </main>
-        </BrowserRouter>
-      )}
     </div>
   );
 }
 
-export default App;
+export default App
