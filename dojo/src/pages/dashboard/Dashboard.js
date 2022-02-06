3de617dace6dac0290e728db0ff8e568102d5b44
johndao1005 <john.d1729@gmail.com>
2022-02-06 20:01:53 +1300

    finish the showing project list

--- dojo/src/pages/dashboard/Dashboard.js
+++ dojo/src/pages/dashboard/Dashboard.js
@@ -1,10 +1,14 @@
 import "./Dashboard.css";
-
+import ProjectList from "../../components/ProjectList"
+import {useCollection} from "../../hooks/useCollection"
 import React from 'react';
 
 function Dashboard() {
+  const {documents,error} = useCollection('projects');
   return <div>
-    
+    <h2 className="page-title">Dashboard</h2>
+    {error && <div className="error">{error}</div>}
+    {documents && <ProjectList projects={(documents)}/>}
   </div>;
 }
 
